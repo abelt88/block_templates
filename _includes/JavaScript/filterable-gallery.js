b36ef1a33f9c7b1aae930647c0ba8c71f5cffed7
abelt88 <adriennebelt@gmail.com>
2025-05-03 23:44:30 -0400

    Editing and cleaning multiple sections

---
+++ _includes/JavaScript/filterable-gallery.js
@@ -0,0 +1,37 @@
+// Filterable Gallery Script Below - Once per Site
+      const gallery = document.querySelectorAll('.filterable-gallery')
+    
+    gallery.forEach(section => {
+      
+    
+      const filterButtons = section.querySelectorAll('.filter-button')
+    
+      const gridItems = section.querySelectorAll('.grid-items')
+    
+      console.log(filterButtons,gridItems)
+    
+      function filterGallery() {
+    
+      filterButtons.forEach(button => {
+        button.classList.remove('active')
+      });
+    
+      this.classList.add('active')
+    
+      let target = this.innerHTML.replace(' ','-')
+    
+        gridItems.forEach(image => {
+          if (image.classList.contains(target)){
+            image.classList.remove('hidden')
+          }
+          else { 
+            image.classList.add('hidden')
+          }
+        } )
+      }
+    
+      filterButtons.forEach(button => {
+        button.addEventListener('click', filterGallery)
+      });
+    
+    });
